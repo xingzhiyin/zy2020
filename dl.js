d6c1aa6f1397c5b9eb7b7fc3339929eb950ae608
xingzhiyin <46856705+xingzhiyin@users.noreply.github.com>
2019-05-07 18:45:02 +0800

    Update dl.js

--- dl.js
+++ dl.js
@@ -1,22 +1,22 @@
 if(navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/ipad|iphone|mac/i)) {
 
 
-document.writeln("<DIV id=\'lovexin122\' style=\'text-align:left;left:0px;position:fixed;top:25%;overflow:hidden;\'>");
+document.writeln("<DIV id=\'lovexin122\' style=\'text-align:left;left:10%;position:fixed;top:25%;overflow:hidden;\'>");
 
 document.writeln("<img border=0 src=https://xingzhiyin.github.io/zy2020/xyz.jpg  width=120 height=120 ><br>");
 document.writeln("</div>");
 
-document.writeln("<DIV id=\'lovexin102\' style=\'text-align:right;right:0px;position:fixed;top:25%;overflow:hidden;\'>");
+document.writeln("<DIV id=\'lovexin102\' style=\'text-align:right;right:10%;position:fixed;top:25%;overflow:hidden;\'>");
 
 document.writeln("<img border=0 src=https://xingzhiyin.github.io/zy2020/xyz.jpg  width=120 height=120 ><br>");}else{
 
 
-document.writeln("<DIV id=\'lovexin122\' style=\'text-align:left;left:0px;position:fixed;top:0px;overflow:hidden;\'>");
+document.writeln("<DIV id=\'lovexin122\' style=\'text-align:left;left:10%;position:fixed;top:0px;overflow:hidden;\'>");
 document.writeln("<img border=0 src=https://xingzhiyin.github.io/zy2020/xyz.jpg  width=120 height=120 ><br>");
 
 document.writeln("</div>");
 
-document.writeln("<DIV id=\'lovexin102\' style=\'text-align:right;right:0px;position:fixed;top:0px;overflow:hidden;\'>");
+document.writeln("<DIV id=\'lovexin102\' style=\'text-align:right;right:10%;position:fixed;top:0px;overflow:hidden;\'>");
 document.writeln("<img border=0 src=https://xingzhiyin.github.io/zy2020/xyz.jpg  width=120 height=120 ><br>");
 document.writeln("</div>");
 
